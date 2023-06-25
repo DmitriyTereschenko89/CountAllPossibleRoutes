07f95419f5d4cfaa3ca11abaafcec621add5e25f
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-06-25 14:09:19 +0300

    Добавьте файлы проекта.

---
+++ CountAllPossibleRoutes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountAllPossibleRoutes
+{
+	internal class CountAllPossibleRoutes
+	{
+		public int CountRoutes(int[] locations, int start, int finish, int fuel)
+		{
+
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CountAllPossibleRoutes
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
