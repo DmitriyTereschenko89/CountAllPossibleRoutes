8ccc69a80fc8152e9221a76b578405587917b43a
Dmitriy <35215775+DimaTer89@users.noreply.github.com>
2023-06-25 14:31:11 +0300

    update read.me add add solution

--- CountAllPossibleRoutes.cs
+++ CountAllPossibleRoutes.cs
@@ -1,16 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CountAllPossibleRoutes
+﻿namespace CountAllPossibleRoutes
 {
 	internal class CountAllPossibleRoutes
 	{
+		private const int modulo = 1000000007;
+
+		private int DFS(int[] locations, int[][] dp, int location, int finish, int fuel)
+		{
+			if (fuel < 0)
+			{
+				return 0;
+			}
+			if (dp[location][fuel] != -1)
+			{
+				return dp[location][fuel];
+			}
+			int routeCount = location == finish ? 1 : 0;
+			for (int nextCity = 0; nextCity < locations.Length; ++nextCity)
+			{
+				if (nextCity != location)
+				{
+					routeCount = (routeCount + DFS(locations, dp, nextCity, finish, fuel - Math.Abs(locations[location] - locations[nextCity]))) % modulo;
+				}
+			}
+			dp[location][fuel] = routeCount;
+			return routeCount;
+		}
 		public int CountRoutes(int[] locations, int start, int finish, int fuel)
 		{
-
+			int n = locations.Length;
+			int[][] dp = new int[n][];
+			for (int i = 0; i < n; ++i)
+			{
+				dp[i] = new int[fuel + 1];
+				Array.Fill(dp[i], -1);
+			}
+			return DFS(locations, dp, start, finish, fuel);
 		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,15 @@
-﻿namespace CountAllPossibleRoutes
+﻿using NUnit.Framework;
+
+namespace CountAllPossibleRoutes
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			CountAllPossibleRoutes countAllPossibleRoutes = new();
+			Assert.AreEqual(4, countAllPossibleRoutes.CountRoutes(new int[] { 2, 3, 6, 8, 4 }, 1, 3, 5));
+			Assert.AreEqual(5, countAllPossibleRoutes.CountRoutes(new int[] { 4, 3, 1 }, 1, 0, 6));
+			Assert.AreEqual(0, countAllPossibleRoutes.CountRoutes(new int[] { 5, 2, 1 }, 0, 2, 3));
 		}
 	}
 }
